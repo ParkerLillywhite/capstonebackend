735a6b05a0f7d3ce6acf2bd82df4ce9b3a40945a
Parker Lillywhite <parkerlillywhite@Parkers-MacBook-Pro.local>
2021-11-10 12:45:56 -0700

    installed cors

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
